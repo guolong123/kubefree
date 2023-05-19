4d66f56f6842f48b8921a7c704d0a6e32d735a27
guolong <guolong@xishuhq.com>
2023-05-19 13:55:51 +0800

    feat: 支持多种时间格式

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -194,7 +194,7 @@ func (c *controller) checkNamespace(ns *v1.Namespace) error {
 
 	lastActivityStatus, err := getActivity(ac)
 	if err != nil {
-		logrus.WithError(err).Fatalln("Error getActivity")
+		logrus.WithError(err).Fatalln("Error getActivity: %v", ac)
 		return err
 	}
 
@@ -230,7 +230,7 @@ func (c *controller) syncSleepAfterRules(namespace *v1.Namespace, lastActivity a
 	}
 
 	state := namespace.Labels[c.ExecutionStateSelector]
-	if time.Since(lastActivity.LastActivityTime) > thresholdDuration {
+	if time.Since(lastActivity.LastActivityTime.Time()) > thresholdDuration {
 		switch state {
 		case DELETING:
 			// do nothing
@@ -244,7 +244,7 @@ func (c *controller) syncSleepAfterRules(namespace *v1.Namespace, lastActivity a
 			}
 			// delete namespace if the namespace still in inactivity status
 			// after thresholdDuration * 2 time
-			if namespace.Status.Phase != v1.NamespaceTerminating && time.Since(lastActivity.LastActivityTime) > thresholdDuration*2 {
+			if namespace.Status.Phase != v1.NamespaceTerminating && time.Since(lastActivity.LastActivityTime.Time()) > thresholdDuration*2 {
 				logrus.WithField("namespace", namespace.Name).
 					WithField("lastActivityTime", lastActivity.LastActivityTime).
 					WithField("sleep-after", v).Info("delete inactivity namespace")
@@ -312,7 +312,7 @@ func (c *controller) syncDeleteAfterRules(namespace *v1.Namespace, lastActivity 
 		return fmt.Errorf("time.ParseDuration failed, label %s, value %s", c.DeleteAfterSelector, v)
 	}
 
-	if time.Since(lastActivity.LastActivityTime) > thresholdDuration && namespace.Status.Phase != v1.NamespaceTerminating {
+	if time.Since(lastActivity.LastActivityTime.Time()) > thresholdDuration && namespace.Status.Phase != v1.NamespaceTerminating {
 		// Delete all PVCs in the namespace
 		pvcList, err := c.clientset.CoreV1().PersistentVolumeClaims(namespace.Name).List(context.Background(), metav1.ListOptions{})
 		if err != nil {
